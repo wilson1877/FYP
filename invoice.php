26554a8e5a31f60bc868d74247dd25c0cd70f7eb
wilson1877 <ckm_5801@yahoo.com>
2018-04-13 01:07:44 +0800

    Ignore

--- invoice.php
+++ invoice.php
@@ -13,23 +13,19 @@ if(isset($_SESSION["userID"]) && !empty($_SESSION["userID"])) {
     $isDriver = $_SESSION['isDriver'];
     $firstname = $_SESSION['firstName'];
 }
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "fyp";
 $con = new mysqli($servername, $username, $password, $dbname);
-
 if (isset($_POST['submitAdd'])) {
 		$customerName = $_POST['customerName'];
 		$purchaseOrderNo = $_POST['purchaseOrderNo'];
 		$miscNotes = $_POST['miscNotes'];
-
 		if (isset($_POST['myCheck'])) {
 			//Checking the associated Customer with their ID via Name
 			$sqlcheckidnumber = "SELECT customerID FROM customer WHERE customerName = '$customerName'"; //Checking for duplicates
 			$runquery = mysqli_query($con, $sqlcheckidnumber);
-
 			if ($runquery -> num_rows <= 0) {
 				//It's a new customer! Adding to the DB!
 				$companyName=$_POST['companyName'];
@@ -37,13 +33,10 @@ if (isset($_POST['submitAdd'])) {
 				$customerFaxNo=$_POST['customerFaxNo'];
 				$customerEmail=$_POST['customerEmail'];
 				$customerAddress=$_POST['customerAddress'];
-
 				$sqlnewcustomerinsert = "INSERT INTO customer(customerName, companyName, contactNumber, faxNumber, emailAddress, address) VALUES ('$customerName', '$companyName', '$customerContactNo', '$customerFaxNo', '$customerEmail', '$customerAddress')";
-
 				$con -> query($sqlnewcustomerinsert);
 			}
 		}
-
 		$sqlcheckidnumber = "SELECT customerID FROM customer WHERE customerName = '$customerName'"; //Checking for duplicates
 		$runquery = mysqli_query($con, $sqlcheckidnumber);
 		if ($runquery -> num_rows > 0) {
@@ -51,35 +44,24 @@ if (isset($_POST['submitAdd'])) {
 			$resultArray = mysqli_fetch_assoc($runquery);
 			$customerID = $resultArray["customerID"];
 			$totalPrice = 0;
-
 			//Adding New Customer
-
 			$currentDate = date("Y/m/d");
-
 			$sqlinsert = "INSERT INTO invoice(totalPrice, customerID, miscNotes, purchaseOrderNo, date) VALUES ('$totalPrice', '$customerID', '$miscNotes', '$purchaseOrderNo', '$currentDate')";
 			$con -> query($sqlinsert);
-
 			$invoiceID = $con->insert_id;
-
 			foreach($_POST['itemName'] as $index => $itemName ) {
 				if ($itemName){
 					$sqlcheckItemName = "SELECT stockID, price FROM stock WHERE stockName = '$itemName'";
 					$runquery2 = mysqli_query($con, $sqlcheckItemName);
-
 					//if ($runquery2 -> num_rows > 0){
 					if (mysqli_num_rows($runquery2) > 0){
 						$resultArray = mysqli_fetch_assoc($runquery2);
-
 						$stockID = $resultArray["stockID"];
 						$price = $resultArray["price"];
-
 						/*var_dump($stockID);
 						var_dump($price);*/
-
 						$itemQuantity = $_POST["itemQuantity"][$index];
-
 						$totalPrice += $price * $itemQuantity;
-
 						$sqlinsert2 = "INSERT INTO invoiceitemlist(invoiceID, stockID, itemQty) VALUES ('$invoiceID', '$stockID', '$itemQuantity')";
 						$con -> query($sqlinsert2);
 					}
@@ -88,13 +70,10 @@ if (isset($_POST['submitAdd'])) {
 			$sqlUpdate = "UPDATE invoice SET totalPrice= '$totalPrice' WHERE invoiceID = '$invoiceID'";
 			$con -> query($sqlUpdate);
 		}else{
-
 		}
 	}
-
 	if(isset($_POST['invoiceView'])){
 		$inputtedID = $_POST['inputtedID'];
-
 		$_SESSION['INPUTTEDID'] = $inputtedID;
 		header('location:invoiceview.php');
 	}
@@ -132,9 +111,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 	</script>
 	<script>
 	        new WOW().init();
-
 			var nextItem = 1;
-
 			function additem(){
                 document.getElementById("items").innerHTML += "<div class=\"row\" id=\"row"+(nextItem+1)+"\"> <div class=\"col-md-8 grid_box1\"> <label>Item Name:</label>" +
 				 "<input type=\"text\" list=\"itemList\" name=\"itemName[]\" id=\"itemName[]\" class=\"form-control1 control3\" /> </div>" +
@@ -143,7 +120,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 				 "<div class=\"col-md-2\"><div><div><button class=\"btn btn-danger invoice-padding\" onClick=\"return removeItemRow('row"+(nextItem+1)+"');\">Remove Row</button></div></div> </div>"+
 				 "<div class=\"clearfix\"> </div></div>";
                 nextItem += 1;
-
                 return false;
             };
 			function removeItemRow(rowID){
@@ -174,7 +150,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 		.invoice-padding{
 			margin-top : 25px;
 		}
-
 		.selected {
 			background-color: brown;
 			color: #FFF;
@@ -357,7 +332,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 									theform.submit()
 								}
 							}
-
 							function removeInvoice(){
 								if (document.getElementById("selectedID").value < 1 ){
 									alert("No Invoice selected");
@@ -370,7 +344,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 									}
 								}
 							}
-
 							function viewInvoice(){
 								if (document.getElementById("selectedID").value < 1 ){
 									alert("No Invoice selected");
@@ -420,7 +393,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 													$password = "";
 													$dbname = "fyp";
 													$con = new mysqli($servername, $username, $password, $dbname);
-
 													$sql = "SELECT * FROM customer";
 													$result = mysqli_query($con, $sql);
 													?>
@@ -480,7 +452,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 															<?php
 															$sql = "SELECT * FROM stock";
 															$result = mysqli_query($con, $sql);
-
 															while($row = mysqli_fetch_array($result)) { ?>
 																<option value="<?php echo $row['stockName']; ?>"><?php echo $row['stockName']; ?></option>
 															<?php } ?>
@@ -607,7 +578,6 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 												<form action="" method="post">
 													<label>Invoice ID:</label>
 													<input type="text" id="inputtedID" name="inputtedID" class="form-control1 control3">
-
 													<button class="btn btn-success" contenteditable="false" name="invoiceView" style="margin-left: 43%;" type="submit">Submit</button>
 												</form>
 											</div>
